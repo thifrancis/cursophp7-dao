c36807917ae776a700a2f20e20f3f83eec501dd4
thifrancis <thiago.francis@hotmail.comgit config --global user.name thiago.francis@hotmail.comgit config --global user.email thiago.francis@hotmail.comgit config --global user.email thiago.francis@hotmail.com>
2020-02-16 13:06:12 -0300

    inicio do projeto

---
+++ class/sql.php
@@ -0,0 +1,68 @@
+<?php 
+
+class Sql extends PDO {
+
+	private $conexao;
+
+	public function __construct (){
+
+				$this->conexao = new PDO ("mysql:host=localhost; dbname=dbphp7", "root","");
+
+
+
+	}
+
+	private function setParams ($stantment, $parameters=array ()){;
+
+			foreach ($parameters as $key => $value) {
+				
+				$stantment->bindParam($key,$value);
+
+				$this->setParam($key,$value);
+
+
+			}
+		}
+
+
+	private function SetParam($statment, $key, $value){
+
+		$statment->bindParam($key,$value);
+
+	}
+	
+
+	public function query($rawQuery, $params = array()){
+
+			$stmt = $this->conexao->prepare($rawQuery);
+
+			$this->setParams($stmt, $params);
+
+		$stmt->execute();
+
+			return $stmt ;
+
+
+	}
+
+
+	public function select ($rawQuery, $params = array()):array
+	{
+
+		$stmt = $this->query($rawQuery, $params);
+
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+
+
+	}
+
+
+}
+
+
+
+
+?>
